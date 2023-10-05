0e4a70bea3ee049dd810075fdd8ced64ce5c0782
wojtan222 <131182605+wojtan222@users.noreply.github.com>
2023-10-05 13:42:47 +0200

    Create README.wc

--- src/test/java/pl/coderslab/shop/NewAddressTest.java
+++ src/test/java/pl/coderslab/shop/NewAddressTest.java
@@ -1,2 +1,11 @@
-package pl.coderslab.shop;public class NewAddressTest {
+package pl.coderslab.shop;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Cucumber/Features",
+        glue = "steps")
+        public class NewAddressTest{
 }
